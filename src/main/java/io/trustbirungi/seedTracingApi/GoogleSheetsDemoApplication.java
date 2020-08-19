c622901d3da10fef105d15bebca6d7b0ee6216d3
Trust Birungi <diamondtrust66@gmail.com>
2020-08-19 11:00:06 +0300

    Initial commit.

---
+++ src/main/java/io/trustbirungi/seedTracingApi/GoogleSheetsDemoApplication.java
@@ -0,0 +1,13 @@
+package io.trustbirungi.seedTracingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSheetsDemoApplication.class, args);
+	}
+
+}

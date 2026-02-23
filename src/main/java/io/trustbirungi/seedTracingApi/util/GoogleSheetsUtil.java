a5e83aaf6b97c3bc0bffff5f24dc4f056f80562b
Trust Birungi <trustbirungi@gmail.com>
2026-02-23 18:29:02 +0300

    Updated Google Sheets dependencies

--- src/main/java/io/trustbirungi/seedTracingApi/util/GoogleSheetsUtil.java
+++ src/main/java/io/trustbirungi/seedTracingApi/util/GoogleSheetsUtil.java
@@ -8,7 +8,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
@@ -30,7 +30,7 @@ import java.util.List;
 //TODO: Check for newer implementation
 public class GoogleSheetsUtil {
 	private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final JsonFactory JSON_FACTORY = new GsonFactory();
 	private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
 	private static Integer rowCount = 0;
